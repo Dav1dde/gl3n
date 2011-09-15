b45f566a592deb0d0570e9e43b16bfc1e8769fe6
David Herberth <admin@dav1d.de>
2011-09-15 21:53:07 +0200

    working glsl like ctor, also some minor bugfixes, thanks to ja_cOp for the ctor

--- gl3n/LinearAlgebra.d
+++ gl3n/LinearAlgebra.d
@@ -10,7 +10,7 @@ struct Vector(type, int dimension_) {
     
     static assert((dimension >= 2) && (dimension <= 4));
     
-    static t[dimension] vector;
+    t[dimension] vector;
 
     @property t x() { return vector[0]; }
     @property void x(t value) { vector[0] = value; }
@@ -48,12 +48,44 @@ struct Vector(type, int dimension_) {
         set('w', w);
     }*/
     
-    this(T)(T vec, t[] args ...) {
+    /*this(T)(T vec, t[] args ...) {
         assert(vec.dimension <= dimension);
         assert((vec.dimension + args.length) == dimension);
     
         vector[0 .. vec.vector.length] = vec.vector;
         vector[vec.vector.length .. $] = args;
+    }*/
+
+    static void isCompatibleVectorImpl(int d)(Vector!(type, d) vec) if(d <= dimension) {
+    }
+
+    template isCompatibleVector(T) {
+        enum isCompatibleVector = is(typeof(isCompatibleVectorImpl(T.init)));
+    }
+
+    private void construct(int i, T, Tail...)(T head, Tail tail) {
+        static if(i >= dimension)
+            static assert(false, "constructor has too many arguments");
+        else static if(is(T : t))
+        {
+            vector[i] = head;
+            construct!(i + 1)(tail);
+        }
+        else static if(isCompatibleVector!T)
+        {   
+            foreach(j; i .. i + T.dimension)
+                vector[j] = head.vector[j-i];
+            construct!(i + T.dimension)(tail);
+        }
+        else
+            static assert(false, "Vector constructor argument must be of type " ~ type.stringof ~ " or Vector, not " ~ T.stringof);
+    }
+    
+    void construct(int i)() { // terminate
+    }
+
+    this(Args...)(Args args) {
+        construct!(0)(args);
     }
     
     @property bool ok() {
@@ -139,10 +171,30 @@ alias Vector!(ubyte, 3) vec3ub;
 alias Vector!(ubyte, 4) vec4ub;
 
 void main() {
-    vec3 f = vec3(1.0f);
-    vec4 b = vec4(f, 0.0f);
-//     b.init_
+    vec3 f;
+    f.clear(1.0f);
+    vec4 b = vec4(1.0f, f);
     writefln("%s", b.vector);
+    writefln("%s", b.init);
+    
+    writefln("---");
+
+    vec2 v1 = vec2(vec2(0.0f, 1.0f));
+    writefln("[0, 1] = %s", v1.vector);
+    
+    vec2 v2 = vec2(1.0f, 1.0f);
+    writefln("[1, 1] = %s", v2.vector);
+    
+    vec3 v3 = vec3(v2, 2.0f);
+    writefln("[1, 1, 2] = %s", v3.vector);
+    
+    vec4 v4 = vec4(1.0f, vec2(2.0f, 3.0f), 4.0f);
+    writefln("[1, 2, 3, 4] = %s", v4.vector);
+    
+    vec4 v5 = vec4(vec2(1.0f, 2.0f), vec2(3.0f, 4.0f));
+    writefln("[1, 2, 3, 4] = %s", v5.vector);
+    
+    writefln("---");
     
     vec4 v = vec4(1.0f, 2.0f, 3.0f, 4.0f);
     writefln("%s", v.x);
