3be1e7c1b2c8e6137e8f94c0ae7786b995580b5b
David Herberth <admin@dav1d.de>
2011-10-27 20:59:16 +0200

    added almost_equal to math

--- gl3n/math.d
+++ gl3n/math.d
@@ -12,13 +12,28 @@ public {
 }
 
 private {
-    import std.math : PI;
+    import std.math : PI, abs;
     import std.conv : to;
 }
 
 public enum real PI_180 = PI / 180;
 public enum real _180_PI = 180 / PI;
 
+bool almost_equal(T, S)(T a, S b, float epsilon = 0.000001f) if(is(T : S)) {
+    if(abs(a-b) <= epsilon) {
+        return true;
+    }
+    return abs(a-b) <= epsilon * abs(b);
+}
+
+unittest {
+    assert(almost_equal(0, 0));
+    assert(almost_equal(1, 1));
+    assert(almost_equal(-1, -1));    
+    assert(almost_equal(0f, 0.000001f, 0.000001f));
+    assert(almost_equal(1f, 1.1f, 0.1f));
+    assert(!almost_equal(1f, 1.1f, 0.01f));
+}
 
 T radians(T)(T degrees) {
     return to!(T)(PI_180 * degrees);
