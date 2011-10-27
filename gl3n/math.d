87c244141cd5c520b24b47bd137a10463f787dc1
David Herberth <admin@dav1d.de>
2011-10-27 21:13:09 +0200

    fixed some little bugs, added unittests

--- gl3n/math.d
+++ gl3n/math.d
@@ -43,8 +43,33 @@ T degrees(T)(T radians) {
     return to!(T)(_180_PI * radians);
 }
 
+unittest {
+    assert(radians(to!(real)(0)) == 0);
+    assert(radians(to!(real)(90)) == PI/2);
+    assert(radians(to!(real)(180)) == PI);
+    assert(radians(to!(real)(360)) == 2*PI);
+    
+    assert(degrees(to!(real)(0)) == 0);
+    assert(degrees(to!(real)(PI/2)) == 90);
+    assert(degrees(to!(real)(PI)) == 180);
+    assert(degrees(to!(real)(2*PI)) == 360);    
+
+    assert(degrees(radians(to!(real)(12))) == 12);
+    assert(degrees(radians(to!(real)(100))) == 100);
+    assert(degrees(radians(to!(real)(213))) == 213);
+    assert(degrees(radians(to!(real)(399))) == 399);
+}
+
 T clamp(T)(T x, T min_val, T max_val) {
     return min(max(x, min_val), max_val);
+}
+
+unittest {
+    assert(clamp(-1, 0, 2) == 0);
+    assert(clamp(0, 0, 2) == 0);
+    assert(clamp(1, 0, 2) == 1);
+    assert(clamp(2, 0, 2) == 2);
+    assert(clamp(3, 0, 2) == 2);
 }
 
 float step(T)(T edge, T x) {
@@ -52,6 +77,19 @@ float step(T)(T edge, T x) {
 }
 
 T smoothstep(T)(T edge0, T edge1, T x) {
-    T t = clamp((x - edge0) / (edge1 - edge0), 0, 1);
+    T t = clamp((x - edge0) / (edge1 - edge0), to!(T)(0), to!(T)(1));
     return t * t * (3 - 2 * t);
+}
+
+unittest {
+    assert(step(0, 1) == 1.0f);
+    assert(step(0, 10) == 1.0f);
+    assert(step(1, 0) == 0.0f);
+    assert(step(10, 0) == 0.0f);
+    assert(step(1, 1) == 1.0f);
+    
+    assert(smoothstep(1, 0, 2) == 0);
+    assert(smoothstep(1.0, 0.0, 2.0) == 0);
+    assert(smoothstep(1.0, 0.0, 0.5) == 0.5);
+    assert(almost_equal(smoothstep(0.0, 2.0, 0.5), 0.15625, 0.00001));
 }
