1125524cb7aead5d2546344b5f4915f2ee755a62
David Herberth <admin@dav1d.de>
2011-12-04 22:59:19 +0100

    added documentation to gl3n.util

--- gl3n/util.d
+++ gl3n/util.d
@@ -10,18 +10,21 @@ private {
 
 private void is_vector_impl(T, int d)(Vector!(T, d) vec) {}
 
+/// If T is a vector, this evaluates to true, otherwise false.
 template is_vector(T) {
     enum is_vector = is(typeof(is_vector_impl(T.init)));
 }
 
 private void is_matrix_impl(T, int r, int c)(Matrix!(T, r, c) mat) {}
 
+/// If T is a matrix, this evaluates to true, otherwise false.
 template is_matrix(T) {
     enum is_matrix = is(typeof(is_matrix_impl(T.init)));
 }
 
 private void is_quaternion_impl(T)(Quaternion!(T) qu) {}
 
+/// If T is a quaternion, this evaluates to true, otherwise false.
 template is_quaternion(T) {
     enum is_quaternion = is(typeof(is_quaternion_impl(T.init)));
 }
