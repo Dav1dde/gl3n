b7ed7dfc66e572d5e7d802015e0425269bef09b3
dayllenger <dayllenger@protonmail.com>
2017-11-07 08:50:48 +0300

    added unittest for Frustum.intersect

--- gl3n/frustum.d
+++ gl3n/frustum.d
@@ -119,6 +119,41 @@ struct Frustum {
         return result;
     }
 
+    unittest {
+        mat4 view = mat4.look_at(vec3(0), vec3(0, 0, 1), vec3(0, 1, 0));
+        enum aspect = 4.0/3.0;
+        enum fov = 60;
+        enum near = 1;
+        enum far = 100;
+        mat4 proj = mat4.perspective(aspect, 1.0, fov, near, far);
+        auto f = Frustum(proj * view);
+        assert(f.intersects(AABB(vec3(0, 0, 1), vec3(0, 0, 1))) == INSIDE);
+        assert(f.intersects(AABB(vec3(-1), vec3(1))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(-1), vec3(0.99))) == OUTSIDE);
+        assert(f.intersects(AABB(vec3(-1000), vec3(1000))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(0, 0, -1000), vec3(1, 1, 1000))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(-1000, 0, 0), vec3(1000, 0.1, 0.1))) == OUTSIDE);
+        for(int i = near; i < far; i += 10) {
+            assert(f.intersects(AABB(vec3(0, 0,  i), vec3(0.1, 0.1,   i + 1))) == INSIDE);
+            assert(f.intersects(AABB(vec3(0, 0, -i), vec3(0.1, 0.1, -(i + 1)))) == OUTSIDE);
+        }
+        import std.math : tan;
+        float c = aspect * far / tan(cradians!fov);
+        assert(f.intersects(AABB(vec3(c, 0, 99), vec3(c + 1, 1, 101))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(c - 4, 0, 98), vec3(c - 2, 1, 99.99))) == INSIDE);
+        assert(f.intersects(AABB(vec3(c, 0, 100), vec3(c + 1, 0, 101))) == OUTSIDE);
+
+        proj = mat4.orthographic(-aspect, aspect, -1.0, 1.0, 0, far);
+        f = Frustum(proj * view);
+        assert(f.intersects(AABB(vec3(0, 0, 1), vec3(0, 0, 1))) == INSIDE);
+        assert(f.intersects(AABB(vec3(-1), vec3(1))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(-1), vec3(0.01))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(0, 0, far - 5), vec3(1, 1, far))) == INSIDE);
+        assert(f.intersects(AABB(vec3(0, 0, far - 5), vec3(1, 1, far + 5))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(-1000, 0, -0.01), vec3(1000, 1, 0))) == INTERSECT);
+        assert(f.intersects(AABB(vec3(-1000, 0, -0.02), vec3(1000, 1, -0.01))) == OUTSIDE);
+    }
+
     /// Returns true if the $(I aabb) intersects with the frustum or is inside it.
     bool opBinaryRight(string s : "in")(AABB aabb) const {
         return intersects(aabb) > 0;
